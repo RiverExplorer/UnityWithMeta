ad07adb56c6b95f039da0b010b44ce2db98f002b
Doug Royer <RiverExplorer.US@gmail.com>
2025-04-22 11:15:00 -0700

    Added a default scene loader script to Unity Editor. Set the default scene to load when the editor starts. Stops it from loading an empty non existant scene.

---
+++ SetDefaultEditorScene/Editor/DefaultSceneLoader.cs
@@ -0,0 +1,37 @@
+/**
+ * Project: Phoenix
+ * Time-stamp: <2025-03-24 14:02:35 doug>
+ *
+ * @file DefaultSceneLoader.cs
+ * @author Douglas Mark Royer
+ * @date 24-FEB-2025
+ *
+ * @Copyright(C) 2025 by Douglas Mark Royer (A.K.A. RiverExplorer)
+ *
+ * Licensed under the MIT License. See LICENSE file
+ * or https://opensource.org/licenses/MIT for details.
+ *
+ * RiverExplorer is a trademark of Douglas Mark Royer
+ *
+ * Unless otherwise specified, all of this code is original
+ * code by the author.
+ */
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class DefaultSceneLoader
+{
+    static DefaultSceneLoader()
+    {
+        EditorApplication.update += LoadDefaultScene;
+    }
+
+    static void LoadDefaultScene()
+    {
+        if (EditorApplication.isPlayingOrWillChangePlaymode)
+            return;
+        EditorApplication.update -= LoadDefaultScene;
+        EditorSceneManager.OpenScene("Assets/Scenes/YOURSCENE.unity");
+    }
+}
